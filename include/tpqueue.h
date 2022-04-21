6a3c057bcbe99c976c00f618e2574c4ef8a8fd8f
Алексей Смирнов <99120962+AleksGerich@users.noreply.github.com>
2022-04-21 11:37:57 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -11,7 +11,6 @@ class TPQueue {
  public:
      TPQueue() : begin(0), end(0), count(0) { }
      void push(const T item) {
-         bool flag = false;
          for (int i = end; i >= begin; i--) {
              if (item.prior > arr[(i - 1) % size].prior && i > begin) {
                  arr[i % size] = arr[(i - 1) % size];
